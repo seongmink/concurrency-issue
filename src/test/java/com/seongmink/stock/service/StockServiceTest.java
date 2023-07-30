939d4a9df5769c1f1019d4b877f2c7b4e2264a0c
Kim Seongmin <sminggo5@naver.com>
2023-07-30 21:02:43 +0900

    동시성 문제 발생 테스트 작성

--- src/test/java/com/seongmink/stock/service/StockServiceTest.java
+++ src/test/java/com/seongmink/stock/service/StockServiceTest.java
@@ -3,11 +3,16 @@ package com.seongmink.stock.service;
 import com.seongmink.stock.domain.Stock;
 import com.seongmink.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class StockServiceTest {
@@ -36,6 +41,28 @@ public class StockServiceTest {
 
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
-        Assertions.assertEquals(99, stock.getQuantity());
+        assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void stock_decrease_100_request() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
     }
 }
